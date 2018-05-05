d61388b4f7afbaaee62ba5de73d0dd02ef1897b3
Vilém Kužel <39015391+Vilda007@users.noreply.github.com>
2018-05-06 00:48:11 +0200

    iTRUBEC03 - Wemos D1 mini data logger with OLED

---
+++ iTRUBEC03-Wemos-D1-mini-logger-with-OLED.ino
@@ -0,0 +1,146 @@
+long lastJob1s = 0, lastJob30s = 0, lastJob1min = 0;
+float myTeplota, myTlak, myVlhkost;
+int myZvuk;
+const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+// připojení potřebných knihoven
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// nastavení adresy senzoru
+#define BME280_ADRESA (0x76)
+// inicializace senzoru BME z knihovny
+Adafruit_BME280 bme;
+
+// nastavení OLED
+#define OLED_RESET 0  // GPIO0
+// inicializace OLED
+Adafruit_SSD1306 display(OLED_RESET);
+
+//SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-SETUP-
+void setup() {
+  // komunikace po sériové lince rychlostí 9600 baud
+  Serial.begin(9600);
+  // zahájení komunikace se senzorem BME280,
+  // v případě chyby je vypsána hláška po sériové lince
+  // a zastaven program
+  if (!bme.begin(BME280_ADRESA)) {
+    Serial.println("BME280 senzor nenalezen, zkontrolujte zapojeni!");
+    while (1);
+  }
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(2000);
+  // Clear the buffer.
+  display.clearDisplay();
+
+  ReadBME();
+
+  //LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-LOOP-
+}
+void loop() {
+
+  SamplujZvuk();
+  Vypis();
+
+  //LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s-LOOP-BLOCK-1s
+  if (millis() > (1000 + lastJob1s))
+  {
+    // kód vykonaný každou 1 vteřinu (1000 ms)
+
+
+    lastJob1s = millis();
+  }
+
+  //LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-LOOP-BLOCK-30s-
+  if (millis() > (30000 + lastJob30s))
+  {
+    // kód vykonaný každých 30 vteřin (30000 ms)
+    ReadBME();
+
+    lastJob30s = millis();
+  }
+
+  //LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min-LOOP-BLOCK-1min
+  if (millis() > (60000 + lastJob1min))
+  {
+    // kód vykonaný každou 1 minutu (60000 ms)
+
+
+    lastJob1min = millis();
+  }
+
+  //LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-LOOP-BLOCK-
+}
+//FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS - FUNCTIONS -
+void Vypis() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 1);
+  display.print("zvuk:");
+  display.println(myZvuk);
+  display.drawLine(0, 0, 63, 0, BLACK);
+  display.drawLine(0, 0, (myZvuk / 16) - 1, 0, WHITE);
+  display.setCursor(0, 10);
+  display.print("T: ");
+  display.print(myTeplota);
+  display.println(" C");
+  //display.setCursor(0, 20);
+  display.print("V: ");
+  display.print(int(myVlhkost));
+  display.println(" %");
+  //display.setCursor(0, 30);
+  display.print("T: ");
+  display.print(int(myTlak));
+  display.println(" hPa");
+  display.display();
+  //delay(1);
+}
+
+void ReadBME() {
+  // čtení všech dostupných informací ze senzoru BMP
+  // teplota
+  myTeplota = (bme.readTemperature());
+  // relativní vlhkost
+  myVlhkost = (bme.readHumidity());
+  // tlak s přepočtem na hektoPascaly
+  myTlak = (bme.readPressure() / 100.0F);
+}
+
+void SamplujZvuk(){
+  unsigned long startMillis = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow)
+  {
+    sample = analogRead(A0);
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax)
+      {
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+  myZvuk = peakToPeak;
+  //Serial.println(volts);
+  }
